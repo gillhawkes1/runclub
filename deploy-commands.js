3b1df4607d860d23402c0f44c3ff372fee1d5ef8
Gill Hawkes <gillhawkes1@gmail.com>
2022-07-31 14:20:01 -0400

    packages and inital login code

---
+++ deploy-commands.js
@@ -0,0 +1,23 @@
+//a file for command deployment management
+
+const { SlashCommandBuilder, Routes } = require('discord.js');
+const { REST } = require('@discordjs/rest');
+
+
+//do i need to define vars commented out?
+require("dotenv").config();
+//const { CLIENT_ID, GUILD_ID, DISCORD_TOKEN } = 
+
+
+const commands = [
+	new SlashCommandBuilder().setName('ping').setDescription('Replies with pong!'),
+	new SlashCommandBuilder().setName('server').setDescription('Replies with server info!'),
+	new SlashCommandBuilder().setName('user').setDescription('Replies with user info!'),
+]
+	.map(command => command.toJSON());
+
+const rest = new REST({ version: '10' }).setToken(DISCORD_TOKEN);
+
+rest.put(Routes.applicationGuildCommands(CLIENT_ID, GUILD_ID), { body: commands })
+	.then(() => console.log('Successfully registered application commands.'))
+	.catch(console.error);
