aa8df4887f881c1c46feda5158eed97d9775199e
Gill Hawkes <gillhawkes1@gmail.com>
2022-08-02 16:47:42 -0400

    index.js changes. it is being hardstop on the Collection() creation rn

--- index.js
+++ index.js
@@ -16,8 +16,6 @@ const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('
 for (const file of commandFiles) {
 	const filePath = path.join(commandsPath, file);
 	const command = require(filePath);
-	// Set a new item in the Collection
-	// With the key as the command name and the value as the exported module
 	client.commands.set(command.data.name, command);
 }
 
