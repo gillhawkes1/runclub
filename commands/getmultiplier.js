9a42898e2f6ec9ff7dd18290b08f68738b9cea91
Gill Hawkes <gillhawkes1@gmail.com>
2022-08-30 23:43:47 -0400

    added commands to get and set the current multiplier

---
+++ commands/getmultiplier.js
@@ -0,0 +1,22 @@
+const { SlashCommandBuilder } = require('discord.js');
+const util = require('./../utility.js');
+const { sd } = require('./../staticdata.js');
+
+
+
+//protected vars import
+const varfile = require('dotenv');
+const configfile = varfile.config();
+const env = configfile.parsed;
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('getmultiplier')
+		.setDescription('Get the multiplier for runs.'),
+	async execute(interaction) {
+        if(util.isRole(interaction, 'Admin') == false){
+            return interaction.reply('You are not an admin.');
+        }
+        return interaction.reply({content: 'Multiplier is currently set to ' + sd.runData.multiplier, ephemeral: true});
+    }
+}
---
+++ commands/setmultiplier.js
@@ -0,0 +1,30 @@
+const { SlashCommandBuilder } = require('discord.js');
+const util = require('./../utility.js');
+const { sd } = require('./../staticdata.js');
+
+
+
+//protected vars import
+const varfile = require('dotenv');
+const configfile = varfile.config();
+const env = configfile.parsed;
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('setmultiplier')
+		.setDescription('Set the multiplier for runs.')
+        .addNumberOption(option => 
+            option.setName('multiplier')
+            .setDescription('multiplier amount')
+            .setRequired(true)),
+	async execute(interaction) {
+        if(util.isRole(interaction, 'Admin') == false){
+            return interaction.reply('You are not an admin.');
+        }
+        const m = interaction.options.getNumber('multiplier');
+        let res = util.setMultiplier(m);
+        console.log(res);
+
+        return interaction.reply({content: 'Multiplier is now set to ' + m, ephemeral: true});
+    }
+}
