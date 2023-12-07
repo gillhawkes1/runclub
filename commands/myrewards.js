6070b28759debcada88aad686d46dfd27bf8c735
Gill Hawkes <gillhawkes1@gmail.com>
2023-12-07 17:32:09 -0500

    added logic for granting rewards during /newrun, added logic to /myrewards for the return message

--- commands/myrewards.js
+++ commands/myrewards.js
@@ -20,22 +20,36 @@ module.exports = {
           const row = lifetimeRows[i];
           if (row.user_id === interaction.user.id) {
             let myMilestones = JSON.parse(row.milestones);
-            let rewardsMsg = "";
+            let rewardsMsg = `Lifetime miles: ${row.lifetime}\n`;
 
             for (const reward of myMilestones) {
               if (reward.earned && !reward.spent) {
-                rewardsMsg += `:white_check_mark: **${reward.miles}mi**: ${reward.text}\n`;
+                rewardsMsg += `:o: **${reward.miles}mi**: ${reward.text}\n`;
               } else if (reward.earned && reward.spent) {
-                rewardsMsg += `:x: **${reward.miles}mi**: ~~${reward.text}~~\n`;
+                rewardsMsg += `:white_check_mark: **${reward.miles}mi**: ~~${reward.text}~~\n`;
               }
             }
 
-            // return rewards or message with no rewards earned
-            if (rewardsMsg !== "") {
+            // add next reward, return rewards or message with no rewards earned
+            if (rewardsMsg !== `Lifetime miles: ${row.lifetime}\n`) {
+              let milestones = myMilestones.map((reward) => { return reward.miles });
+              // new obj to message if they have hit all of the rewards in the default obj
+              const newRewardsObj = {
+                miles: milestones[milestones.length - 1] + 100,
+                text: "$20 ACBC gift card",
+              }
+
+              milestones.push(parseFloat(row.lifetime));
+              milestones.sort(function(a, b) {return a - b});
+              const nextRewardIndex = milestones.indexOf(parseFloat(row.lifetime)) + 1;
+              const nextRewardObj = myMilestones.find((milestone) => { return milestone.miles === milestones[nextRewardIndex]}) || newRewardsObj;
+              // TODO: Fix this to send the next correct tier of rewards if they are at the end of the default array (1000miles)
+              rewardsMsg += `:lock: Next Reward - **${nextRewardObj.miles}mi**: ${nextRewardObj.text}\n`;
+
               return interaction.reply(rewardsMsg);
             } else {
               return interaction.reply(
-                "You have not received any rewards yet. Keep on running and Heife will reward you with free stuff! :man_running: :woman_running: :person_running:"
+                "You have not received any rewards yet, check out what you can earn by using the **/rewards** command. Keep on running and Heife will reward you with free stuff! :man_running: :woman_running: :person_running:"
               );
             }
           }
